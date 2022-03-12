1d84af075ffae01ec5c555a894c8d892e9561677
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-03-12 18:13:16 -0500

    added loading skeleton and adjusted dashboard padding

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Grid, Paper, Typography } from '@mui/material'
+import { Grid, Paper, Typography, Skeleton } from '@mui/material'
 import { IDigestedWeatherDay } from '../types'
 import WeatherIcon from './WeatherIcon'
 
@@ -8,52 +8,60 @@ interface IComponentProps {
 
 const Dashboard = (props: IComponentProps) => {
   const { forecast } = props
-  if (forecast.length) {
-    return (
-      <Grid container item justifyContent="center" sx={{ marginTop: '1em' }}>
-        <Paper
-          sx={{
-            padding: '0.25em',
-            width: '100%',
-            maxWidth: '800px',
-            borderRadius: '15px',
-          }}
-          elevation={3}
-        >
-          <Grid container item direction="column">
-            <Grid
-              item
-              lg={12}
-              sx={{
-                background: '#EEF6FB',
-                padding: '2em',
-                borderRadius: '15px 15px 0 0',
-                border: '0.25em #FFFFFF solid',
-              }}
-            >
-              <Grid container item direction="column">
-                <Grid container item justifyContent="center">
-                  <Grid item>
+  return (
+    <Grid container item justifyContent="center" sx={{ marginTop: '1em' }}>
+      <Paper
+        sx={{
+          padding: '0.25em',
+          width: '100%',
+          maxWidth: '800px',
+          borderRadius: '15px',
+        }}
+        elevation={3}
+      >
+        <Grid container item direction="column">
+          <Grid
+            item
+            lg={12}
+            sx={{
+              background: '#EEF6FB',
+              padding: '3em 0 3em 0',
+              borderRadius: '15px 15px 0 0',
+              border: '0.25em #FFFFFF solid',
+            }}
+          >
+            <Grid container item direction="column">
+              <Grid container item justifyContent="center">
+                <Grid item>
+                  {forecast.length ? (
                     <Typography variant="h4" sx={{ fontWeight: 'light' }}>
                       Today
                     </Typography>
-                  </Grid>
+                  ) : (
+                    <Skeleton variant="text" width={130} height={42} />
+                  )}
                 </Grid>
-                <Grid item container justifyContent="center">
-                  <Grid item container lg={3} justifyContent="center">
+              </Grid>
+              <Grid item container justifyContent="center">
+                <Grid item container lg={3} justifyContent="center">
+                  {forecast.length ? (
                     <WeatherIcon
                       weatherId={forecast[0].weatherId}
                       size={'10rem'}
                     />
-                  </Grid>
-                  <Grid
-                    item
-                    container
-                    direction="column"
-                    lg={2}
-                    alignItems="center"
-                  >
-                    <Grid item>
+                  ) : (
+                    <Skeleton variant="rectangular" width={140} height={150} />
+                  )}
+                </Grid>
+                <Grid
+                  item
+                  container
+                  direction="column"
+                  lg={2}
+                  alignItems="center"
+                >
+                  <Grid item>
+                    {forecast.length ? (
                       <Typography
                         sx={{
                           fontWeight: 'bold',
@@ -63,43 +71,59 @@ const Dashboard = (props: IComponentProps) => {
                       >
                         {forecast[0].temp}°
                       </Typography>
-                    </Grid>
-                    <Grid item>
+                    ) : (
+                      <Skeleton variant="text" width={80} height={95} />
+                    )}
+                  </Grid>
+                  <Grid item>
+                    {forecast.length ? (
                       <Typography
                         sx={{ fontSize: '2rem', fontWeight: 'light' }}
                       >
                         {forecast[0].weather}
                       </Typography>
-                    </Grid>
+                    ) : (
+                      <Skeleton variant="text" width={80} height={50} />
+                    )}
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
-            <Grid container item>
-              <Grid
-                container
-                item
-                lg={3}
-                sx={{
-                  background: '#EEF6FB',
-                  borderRadius: '0 0 0 15px',
-                  padding: '1em',
-                  border: '0.25em #FFFFFF solid',
-                }}
-              >
-                <Grid container item direction="column" alignItems="center">
-                  <Grid item>
+          </Grid>
+          <Grid container item>
+            <Grid
+              container
+              item
+              lg={3}
+              sx={{
+                background: '#EEF6FB',
+                borderRadius: '0 0 0 15px',
+                padding: '2em 0 2em 0',
+                border: '0.25em #FFFFFF solid',
+              }}
+            >
+              <Grid container item direction="column" alignItems="center">
+                <Grid item>
+                  {forecast.length ? (
                     <Typography sx={{ fontSize: '1.5em', fontWeight: 'light' }}>
                       {forecast[1].day}
                     </Typography>
-                  </Grid>
-                  <Grid item>
+                  ) : (
+                    <Skeleton variant="text" width={70} height={35} />
+                  )}
+                </Grid>
+                <Grid item>
+                  {forecast.length ? (
                     <WeatherIcon
                       weatherId={forecast[1].weatherId}
                       size={'4rem'}
                     />
-                  </Grid>
-                  <Grid item>
+                  ) : (
+                    <Skeleton variant="rectangular" width={70} height={70} />
+                  )}
+                </Grid>
+                <Grid item>
+                  {forecast.length ? (
                     <Typography
                       sx={{
                         fontWeight: 'bold',
@@ -109,32 +133,44 @@ const Dashboard = (props: IComponentProps) => {
                     >
                       {forecast[1].temp}°
                     </Typography>
-                  </Grid>
+                  ) : (
+                    <Skeleton variant="text" width={70} height={45} />
+                  )}
                 </Grid>
               </Grid>
-              <Grid
-                container
-                item
-                lg={3}
-                sx={{
-                  background: '#EEF6FB',
-                  padding: '1em',
-                  border: '0.25em #FFFFFF solid',
-                }}
-              >
-                <Grid container item direction="column" alignItems="center">
-                  <Grid item>
+            </Grid>
+            <Grid
+              container
+              item
+              lg={3}
+              sx={{
+                background: '#EEF6FB',
+                padding: '2em 0 2em 0',
+                border: '0.25em #FFFFFF solid',
+              }}
+            >
+              <Grid container item direction="column" alignItems="center">
+                <Grid item>
+                  {forecast.length ? (
                     <Typography sx={{ fontSize: '1.5em', fontWeight: 'light' }}>
                       {forecast[2].day}
                     </Typography>
-                  </Grid>
-                  <Grid item>
+                  ) : (
+                    <Skeleton variant="text" width={70} height={35} />
+                  )}
+                </Grid>
+                <Grid item>
+                  {forecast.length ? (
                     <WeatherIcon
                       weatherId={forecast[2].weatherId}
                       size={'4rem'}
                     />
-                  </Grid>
-                  <Grid item>
+                  ) : (
+                    <Skeleton variant="rectangular" width={70} height={70} />
+                  )}
+                </Grid>
+                <Grid item>
+                  {forecast.length ? (
                     <Typography
                       sx={{
                         fontWeight: 'bold',
@@ -144,32 +180,45 @@ const Dashboard = (props: IComponentProps) => {
                     >
                       {forecast[2].temp}°
                     </Typography>
-                  </Grid>
+                  ) : (
+                    <Skeleton variant="text" width={70} height={45} />
+                  )}
                 </Grid>
               </Grid>
-              <Grid
-                container
-                item
-                lg={3}
-                sx={{
-                  background: '#EEF6FB',
-                  padding: '1em',
-                  border: '0.25em #FFFFFF solid',
-                }}
-              >
-                <Grid container item direction="column" alignItems="center">
-                  <Grid item>
+            </Grid>
+            <Grid
+              container
+              item
+              lg={3}
+              sx={{
+                background: '#EEF6FB',
+                borderRadius: '0 0 0 15px',
+                padding: '2em 0 2em 0',
+                border: '0.25em #FFFFFF solid',
+              }}
+            >
+              <Grid container item direction="column" alignItems="center">
+                <Grid item>
+                  {forecast.length ? (
                     <Typography sx={{ fontSize: '1.5em', fontWeight: 'light' }}>
                       {forecast[3].day}
                     </Typography>
-                  </Grid>
-                  <Grid item>
+                  ) : (
+                    <Skeleton variant="text" width={70} height={35} />
+                  )}
+                </Grid>
+                <Grid item>
+                  {forecast.length ? (
                     <WeatherIcon
                       weatherId={forecast[3].weatherId}
                       size={'4rem'}
                     />
-                  </Grid>
-                  <Grid item>
+                  ) : (
+                    <Skeleton variant="rectangular" width={70} height={70} />
+                  )}
+                </Grid>
+                <Grid item>
+                  {forecast.length ? (
                     <Typography
                       sx={{
                         fontWeight: 'bold',
@@ -179,33 +228,45 @@ const Dashboard = (props: IComponentProps) => {
                     >
                       {forecast[3].temp}°
                     </Typography>
-                  </Grid>
+                  ) : (
+                    <Skeleton variant="text" width={70} height={45} />
+                  )}
                 </Grid>
               </Grid>
-              <Grid
-                container
-                item
-                lg={3}
-                sx={{
-                  background: '#EEF6FB',
-                  borderRadius: '0 0 15px 0',
-                  padding: '1em',
-                  border: '0.25em #FFFFFF solid',
-                }}
-              >
-                <Grid container item direction="column" alignItems="center">
-                  <Grid item>
+            </Grid>
+            <Grid
+              container
+              item
+              lg={3}
+              sx={{
+                background: '#EEF6FB',
+                borderRadius: '0 0 0 15px',
+                padding: '2em 0 2em 0',
+                border: '0.25em #FFFFFF solid',
+              }}
+            >
+              <Grid container item direction="column" alignItems="center">
+                <Grid item>
+                  {forecast.length ? (
                     <Typography sx={{ fontSize: '1.5em', fontWeight: 'light' }}>
                       {forecast[4].day}
                     </Typography>
-                  </Grid>
-                  <Grid item>
+                  ) : (
+                    <Skeleton variant="text" width={70} height={35} />
+                  )}
+                </Grid>
+                <Grid item>
+                  {forecast.length ? (
                     <WeatherIcon
                       weatherId={forecast[4].weatherId}
                       size={'4rem'}
                     />
-                  </Grid>
-                  <Grid item>
+                  ) : (
+                    <Skeleton variant="rectangular" width={70} height={70} />
+                  )}
+                </Grid>
+                <Grid item>
+                  {forecast.length ? (
                     <Typography
                       sx={{
                         fontWeight: 'bold',
@@ -215,15 +276,17 @@ const Dashboard = (props: IComponentProps) => {
                     >
                       {forecast[4].temp}°
                     </Typography>
-                  </Grid>
+                  ) : (
+                    <Skeleton variant="text" width={70} height={45} />
+                  )}
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Paper>
-      </Grid>
-    )
-  } else return null
+        </Grid>
+      </Paper>
+    </Grid>
+  )
 }
 
 export default Dashboard
