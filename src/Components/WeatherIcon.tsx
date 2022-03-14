cf658cedcf62881bf8a40b0d301dee641644d6f5
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-03-13 20:47:49 -0400

    added meta description and aria labels

--- src/Components/WeatherIcon.tsx
+++ src/Components/WeatherIcon.tsx
@@ -20,24 +20,53 @@ const WeatherIcon = (props: IComponentProps) => {
 
   if (weatherId >= 200 && weatherId <= 232) {
     return (
-      <BsCloudLightningRainFill style={{ color: '#30456A', fontSize: size }} />
+      <BsCloudLightningRainFill
+        aria-label="thunder storm"
+        style={{ color: '#30456A', fontSize: size }}
+      />
     )
   } else if (weatherId >= 300 && weatherId <= 321) {
     return (
-      <BsFillCloudDrizzleFill style={{ color: '#30456A', fontSize: size }} />
+      <BsFillCloudDrizzleFill
+        aria-label="drizzle"
+        style={{ color: '#30456A', fontSize: size }}
+      />
     )
   } else if (weatherId >= 500 && weatherId <= 531) {
     return (
-      <BsFillCloudRainHeavyFill style={{ color: '#30456A', fontSize: size }} />
+      <BsFillCloudRainHeavyFill
+        aria-label="Rain"
+        style={{ color: '#30456A', fontSize: size }}
+      />
     )
   } else if (weatherId >= 600 && weatherId <= 622) {
-    return <BsCloudSnowFill style={{ color: '#30456A', fontSize: size }} />
+    return (
+      <BsCloudSnowFill
+        aria-label="snow"
+        style={{ color: '#30456A', fontSize: size }}
+      />
+    )
   } else if (weatherId >= 701 && weatherId <= 781) {
-    return <BsFillCloudHazeFill style={{ color: '#30456A', fontSize: size }} />
+    return (
+      <BsFillCloudHazeFill
+        aria-label="mist, smoke, haze, fog, or dust"
+        style={{ color: '#30456A', fontSize: size }}
+      />
+    )
   } else if (weatherId === 800) {
-    return <BsFillSunFill style={{ color: '#30456A', fontSize: size }} />
+    return (
+      <BsFillSunFill
+        aria-label="clear skies"
+        style={{ color: '#30456A', fontSize: size }}
+      />
+    )
   } else if (weatherId >= 801 && weatherId <= 804) {
-    return <BsClouds style={{ color: '#30456A', fontSize: size }} />
+    return (
+      <BsClouds
+        aria-label="Cloudy"
+        style={{ color: '#30456A', fontSize: size }}
+      />
+    )
   } else return null
 }
 
